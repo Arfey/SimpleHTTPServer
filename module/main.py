c9d6c6ed25b71e910381533326d98e78e564f229
misha.gavela <arfey17.gm@gmial.com>
2017-05-16 02:36:53 +0300

    feat: Added Status object

--- module/main.py
+++ module/main.py
@@ -35,44 +35,79 @@ RESPONSE_BODY_TEMPLATE = """\
 </html>
 """
 
-class SimpleServer:
-    def __init__(self, *, host, port):
+class BaseHTTPStatus:
+    """Singleton object for getting http status"""
+    def __new__(cls, *args, **kwargs):
+        if cls.instance:
+            return cls.instance
+
+        cls.instance = super().__new__(cls, *args, **kwargs)
+
+        return cls.instance
+
+    # Needing for get values, like simple attributes
+    def __getattr__(self, value):
+        if value in self.status:
+            return self.status.get(value)
+
+    instance = None
+
+    status = {
+        "HTTP_200_OK": "200 OK",
+        "HTTP_404_NOT_FOUND": "404 Not found",
+    }
+
+status = BaseHTTPStatus()
+
+class SimpleHTTPServer:
+    def __init__(self, *, host: str, port: str):
         self.host = host
         self.port = port
+        self.socket = None
 
-    def echo_about_run(self):
+    def echo_about_run(self) -> None:
+        """Write in stdout message with info about start server"""
         print(ECHO_MESSAGES_TEMPLATE % dict(port=self.port, host=self.host))
 
-    def start_forever(self):
+    def send_response(self, title: str = 'http server', conent: str = '', status: str = status.HTTP_200_OK) -> None:
+        """Send http responce to client"""
+        body = RESPONSE_BODY_TEMPLATE % dict(
+                    title='Main Title',
+                    content="<h1>Hello world!!!</h1>")
+
+        # include length of body for HTTP\1.1 protocol
+        res = RESPONSE_HEADER_TEMPLATE % dict(
+                    body=body,
+                    status=status,
+                    length=len(body))
+
+        self.socket.send(res.encode())
+        self.socket.close()
+
+    def send_error(status: str=status.HTTP_404_NOT_FOUND) -> None:
+        self.send_response(status=status)
+
+    def start_forever(self) -> None:
         self.echo_about_run()
 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.bind((HOST, PORT))
             sock.listen(LISTEN_LIMIT)
             while True:
-                conn, addr = sock.accept()
-                data = conn.recv(1024)
+                self.socket, addr = sock.accept()
+                data = self.socket.recv(1024)
 
-                print(conn, addr, data)
+                print(self.socket, addr, data)
 
-                body = RESPONSE_BODY_TEMPLATE % dict(
-                        title='Main Title',
-                        content="<h1>Hello world!!!</h1>"
-                    )
-                res = RESPONSE_HEADER_TEMPLATE % dict(
-                        body=body,
-                        status="200 OK",
-                        length=len(body)
-                    )
+                self.send_response()
 
-                conn.send(res.encode())
-                conn.close()
+                
 
 
 if __name__ == "__main__":
     # Create simple server
 
-    server = SimpleServer(host=HOST, port=PORT)
+    server = SimpleHTTPServer(host=HOST, port=PORT)
 
     webbrowser.open("0.0.0.0:%s" % PORT)
 
