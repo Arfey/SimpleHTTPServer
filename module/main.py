661026534536664801d50de9306faeb3ee03e861
misha.gavela <arfey17.gm@gmial.com>
2017-05-16 01:45:34 +0300

    feat: Added bootstrap

--- module/main.py
+++ module/main.py
@@ -4,8 +4,12 @@ import socket
 import webbrowser
 
 
-HOST, PORT = "", 8000
+HOST, PORT = "0.0.0.0", 8000
 LISTEN_LIMIT = 1
+
+
+ECHO_MESSAGES_TEMPLATE = "\nServing HTTP \
+on %(host)s port %(port)s (http://%(host)s:%(port)s) ...\n"
 
 RESPONSE_HEADER_TEMPLATE = """HTTP/1.1 %(status)s
     Content-Length: %(length)s
@@ -19,6 +23,8 @@ RESPONSE_HEADER_TEMPLATE = """HTTP/1.1 %(status)s
 RESPONSE_BODY_TEMPLATE = """\
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 <head>
     <meta charset="UTF-8">
     <title>%(title)s</title>
@@ -30,7 +36,16 @@ RESPONSE_BODY_TEMPLATE = """\
 """
 
 class SimpleServer:
+    def __init__(self, *, host, port):
+        self.host = host
+        self.port = port
+
+    def echo_about_run(self):
+        print(ECHO_MESSAGES_TEMPLATE % dict(port=self.port, host=self.host))
+
     def start_forever(self):
+        self.echo_about_run()
+
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.bind((HOST, PORT))
             sock.listen(LISTEN_LIMIT)
@@ -57,8 +72,8 @@ class SimpleServer:
 if __name__ == "__main__":
     # Create simple server
 
-    server = SimpleServer()
+    server = SimpleServer(host=HOST, port=PORT)
 
-    # webbrowser.open("0.0.0.0:%s" % PORT)
+    webbrowser.open("0.0.0.0:%s" % PORT)
 
     server.start_forever()
